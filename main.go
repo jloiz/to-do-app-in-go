f2865d13fd088eb5ac070efd8804c2d9232457b7
jloiz <local.dev@mail.com>
2025-11-20 10:45:54 +0000

    Add fibre http server

--- main.go
+++ main.go
@@ -4,8 +4,25 @@ import (
 	"fmt"
 
 	"github.com/jloiz/to-do-app-in-go/greeting"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main(){
-	fmt.Println("Welcome,",  greeting.GreetUser())
+
+	user := greeting.GreetUser()
+
+	var greetingStr string;
+	
+	greetingStr =  "Welcome " +  user + ", getting your tasks."
+
+	fmt.Println(greetingStr)
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString(greetingStr)
+	})
+
+	app.Listen(":3000")
+
 }
