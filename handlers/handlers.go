6c4b7e3229cb29e1ed4edc28cc2de3073f1cf30b
jloiz <local.dev@mail.com>
2025-11-23 12:53:20 +0000

    additional routes

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -15,14 +15,26 @@ func Initialise(c *fiber.Ctx) error {
 	return c.SendString(greeting)
 }
 
-func Initialise2(c *fiber.Ctx) error {
-	greeting := Greeting()
-	return c.SendString(greeting)
-}
-
 //ToDo: Should I move todo app items to own package?
 // After made all endpoints, jsonify the default and send
 
 func GetAllTasks(c *fiber.Ctx) error {
 	return c.SendString("All tasks")
+}
+
+func GetTask(c *fiber.Ctx) error {
+	msg := "A single task " + c.Params("task");
+	return c.SendString(msg);
+}
+
+func UpdateTask(c *fiber.Ctx) error {
+	return c.SendString("Update task")
+}
+
+func NewTask(c *fiber.Ctx) error {
+	return c.SendString("New Task")
+}
+
+func DeleteTask(c *fiber.Ctx) error {
+	return c.SendString("Delete Task")
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,6 +9,10 @@ import (
 func SetupRoutes(app *fiber.App) {
 	// Health
 	app.Get("/", handlers.Initialise)
-	app.Get("/demo", handlers.Initialise2)
 
+	app.Get("/api/v1/todo/get_all", handlers.GetAllTasks)
+	app.Get("/api/v1/todo/get/:task", handlers.GetTask)
+	app.Post("/api/v1/todo/update/:task", handlers.UpdateTask)
+	app.Post("/api/v1/todo/new", handlers.NewTask)
+	app.Delete("/api/v1/todo/delete/:task", handlers.DeleteTask)
 }
